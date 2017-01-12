2c012ced4f9e15ded238d2d5dbb557f46a8fad4e
sirgalleto <sebastian.osorio@wizeline.com>
2017-01-12 11:46:09 -0600

    (update) adding shorts without flux

--- src/App.js
+++ src/App.js
@@ -4,31 +4,35 @@ import Header from './components/Header';
 import CreateForm from './components/CreateForm';
 import UrlList from './components/UrlList';
 
+let urls = [
+  {
+    id: 1,
+    name: 'facebook.com',
+    short: 'alssfz',
+    clicks: 1
+  },
+  {
+    id: 2,
+    name: 'google.com',
+    short: 'a24xloa',
+    clicks: 20
+  }
+];
+
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.urls = [
-      {
-        id: 1,
-        name: 'facebook.com',
-        short: 'alssfz',
-        clicks: 1
-      },
-      {
-        id: 2,
-        name: 'google.com',
-        short: 'a24xloa',
-        clicks: 20
-      }
-    ];
+    this.state = {
+      urls: urls
+    };
   }
   render() {
     return (
       <div className="App">
         <Header/>
         <CreateForm onShort={this.shortLink.bind(this)}></CreateForm>
-        <UrlList urls={this.urls}></UrlList>
+        <UrlList urls={this.state.urls}></UrlList>
       </div>
     );
   }
@@ -38,7 +42,14 @@ class App extends Component {
 
     const link = e.target.querySelector('input').value;
 
-    alert(link);
+    this.setState({
+      urls: this.state.urls.concat({
+        id: this.state.urls[this.state.urls.length - 1].id + 1,
+        name: link,
+        short: 'aaaaa',
+        clicks: 20
+      })
+    });
   }
 }
 
