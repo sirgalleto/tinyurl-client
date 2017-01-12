0ad38d2fa2d1c33688ce44a29350e15ed28024db
sirgalleto <sebastian.osorio@wizeline.com>
2017-01-11 18:44:33 -0600

    (update) use a most colored logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import CreateForm from './components/CreateForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header/>
+        <CreateForm onShort={this.shortLink.bind(this)}></CreateForm>
       </div>
     );
+  }
+
+  shortLink(e) {
+    e.preventDefault();
+
+    const link = e.target.querySelector('input').value;
+
+    alert(link);
   }
 }
 
